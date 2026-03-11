8455f7523607a5d3a0cae93995e1b051a3d4007e
elvissiqueira2 <elvissiqueira@gmail.com>
2026-03-11 14:48:12 -0300

    Create hello.js

---
+++ Netlify/functions/hello.js
@@ -0,0 +1,8 @@
+export async function handler() {
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ ok: true, msg: 'Hello from Netlify Functions!' })
+  };
+}
+``
